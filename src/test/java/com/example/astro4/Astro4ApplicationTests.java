3d44076045c43dff60a2ee627685a7f769012089
Yereke <bikeyev.gmail.com>
2022-09-07 16:01:32 +0600

    Initial commit

---
+++ src/test/java/com/example/astro4/Astro4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.astro4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Astro4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
